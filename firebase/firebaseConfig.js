43929961c33828ccfad77882ae41bdcf04cfd715
Hritick Jaiswal <newbiehritick@gmail.com>
2021-11-18 10:32:23 +0530

    Added firebase to project

---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAg_Bmu2NuDQ1LNQef8KMdoDaDG9oAv2T8",
+  authDomain: "disney-clone-b081c.firebaseapp.com",
+  projectId: "disney-clone-b081c",
+  storageBucket: "disney-clone-b081c.appspot.com",
+  messagingSenderId: "791790671512",
+  appId: "1:791790671512:web:6652bc5d25f98ae41922ed",
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import Head from "next/head";
 import { Fragment } from "react";
+import { initializeApp } from "firebase/app";
 
 import "../styles/globals.css";
+import { firebaseConfig } from "../firebase/firebaseConfig";
 
 function MyApp({ Component, pageProps }) {
+  initializeApp(firebaseConfig);
   return (
     <Fragment>
       <Head>
